302f5f97b9ab2d65907eee526c88d4ad93486e9f
lesteven <le_steven@outlook.com>
2018-02-28 15:32:53 -0800

    add redux viewModule modifiers

--- src/common/reduxModules/viewModule.js
+++ src/common/reduxModules/viewModule.js
@@ -13,17 +13,58 @@ const initialState = {
     screenSize: typeof window === 'object'? window.innerWidth: null
 
 }
-//
+
+//reducer
 export const view = (state = initialState, action) => {
     switch(action.type) {
       case updateView:
           return {
-              ...state,
-              screenSize: action.screenSize
+                ...state,
+                screenSize: action.screenSize,
+                showBlock: showBlock(action.screenSize),
+                showFlex: showFlex(action.screenSize)
           }
       default:
           return state;
     }
 }
+
+
+
+// -----------------------------------------------------------//
+
+
+
+
+// css state based on screen width
+const maxSize = 812;
+
+const showFlex = (size) => {
+    return size < maxSize? 'none' : 'flex';
+}
+
+const showBlock = (size) => {
+    return size < maxSize? 'none': 'block';
+}
+
+// toggle display
+const toggleFlex = (size, display) => {
+    if (size < maxSize) {
+        return display == 'none'? 'flex' : 'none';
+    }
+    return 'flex';
+}
+
+const toggleBlock = (size, display) => {
+    if (size < maxSize) {
+        return display == 'none'? 'block' : 'none';
+    }
+    return 'block';
+}
+
+
+
+
+
 
 export default view;
